3a48f7e48677ab9f4bc7b31a205e96e07f4dcbd6
Dustin Blackman <dev@dustinblackman.com>
2017-03-19 12:01:11 -0400

    expose express router for wechat

--- broid-wechat/lib/core/adapter.js
+++ broid-wechat/lib/core/adapter.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Promise = require("bluebird");
 const broid_schemas_1 = require("broid-schemas");
 const broid_utils_1 = require("broid-utils");
+const crypto = require("crypto");
+const events_1 = require("events");
+const express_1 = require("express");
 const fs = require("fs-extra");
 const uuid = require("node-uuid");
 const path = require("path");
@@ -13,17 +16,13 @@ const tmp = require("tmp");
 const WeChat = require("wechat-api");
 const parser_1 = require("./parser");
 const webHookServer_1 = require("./webHookServer");
-const HTTPOptions = {
-    host: "127.0.0.1",
-    port: 8080,
-};
 class Adapter {
     constructor(obj) {
         this.serviceID = obj && obj.serviceID || uuid.v4();
         this.logLevel = obj && obj.logLevel || "info";
         this.appID = obj && obj.appID;
         this.appSecret = obj && obj.appSecret;
-        this.HTTPOptions = obj.http || HTTPOptions;
+        this.emitter = new events_1.EventEmitter();
         this.logger = new broid_utils_1.Logger("adapter", this.logLevel);
         if (!this.appID) {
             throw new Error("appID must be set");
@@ -33,6 +32,10 @@ class Adapter {
         }
         this.client = Promise.promisifyAll(new WeChat(this.appID, this.appSecret));
         this.parser = new parser_1.default(this.client, this.serviceID, this.logLevel);
+        this.router = this.setupRouter();
+        if (obj.http) {
+            this.webhookServer = new webHookServer_1.default(obj.http, this.router, this.logLevel);
+        }
     }
     serviceId() {
         return this.serviceID;
@@ -41,20 +44,24 @@ class Adapter {
         if (this.connected) {
             return Rx_1.Observable.of({ type: "connected", serviceID: this.serviceId() });
         }
-        this.webhookServer = new webHookServer_1.default(this.serviceID, this.HTTPOptions, this.logLevel);
-        this.webhookServer.listen();
+        if (this.webhookServer) {
+            this.webhookServer.listen();
+        }
         this.connected = true;
         return Rx_1.Observable.of(({ type: "connected", serviceID: this.serviceId() }));
     }
     disconnect() {
         this.connected = false;
-        return this.webhookServer.close();
+        if (this.webhookServer) {
+            return this.webhookServer.close();
+        }
+        return Promise.resolve(null);
     }
     listen() {
         if (!this.webhookServer) {
             return Rx_1.Observable.throw(new Error("No webhookServer found."));
         }
-        return Rx_1.Observable.fromEvent(this.webhookServer, "message")
+        return Rx_1.Observable.fromEvent(this.emitter, "message")
             .mergeMap((event) => this.parser.parse(event))
             .mergeMap((parsed) => this.parser.validate(parsed))
             .mergeMap((validated) => {
@@ -68,6 +75,9 @@ class Adapter {
         return this.client.getFollowersAsync()
             .then((res) => this.client.batchGetUsersAsync(res.data.openid))
             .then(R.prop("user_info_list"));
+    }
+    getRouter() {
+        return this.router;
     }
     send(data) {
         this.logger.debug("sending", { message: data });
@@ -120,6 +130,23 @@ class Adapter {
             }
             return res.media_id;
         });
+    }
+    setupRouter() {
+        const router = express_1.Router();
+        router.get("/", (req, res) => {
+            const shasum = crypto.createHash("sha1");
+            shasum.update([this.serviceID, req.query.timestamp, req.query.nonce].sort().join(""));
+            const signature = shasum.digest("hex");
+            if (signature !== req.query.signature) {
+                return res.status(500).end();
+            }
+            res.status(200).send(req.query.echostr);
+        });
+        router.post("/", (req, res) => {
+            this.emitter.emit("message", req.body.xml);
+            res.status(200).end();
+        });
+        return router;
     }
 }
 exports.default = Adapter;
--- broid-wechat/lib/core/webHookServer.js
+++ broid-wechat/lib/core/webHookServer.js
@@ -3,18 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Promise = require("bluebird");
 const bodyParser = require("body-parser");
 const broid_utils_1 = require("broid-utils");
-const crypto = require("crypto");
-const events_1 = require("events");
 const express = require("express");
 const xmlParser = require("express-xml-bodyparser");
-class WebHookServer extends events_1.EventEmitter {
-    constructor(serviceID, options, logLevel) {
-        super();
+class WebHookServer {
+    constructor(options, router, logLevel) {
         this.host = options.host;
         this.port = options.port;
-        this.serviceID = serviceID;
         this.logger = new broid_utils_1.Logger("webhook_server", logLevel);
-        this.setupExpress();
+        this.setupExpress(router);
     }
     listen() {
         this.httpClient = this.express.listen(this.port, this.host, () => {
@@ -24,25 +20,11 @@ class WebHookServer extends events_1.EventEmitter {
     close() {
         return Promise.fromCallback((cb) => this.httpClient.close(cb));
     }
-    setupExpress() {
+    setupExpress(router) {
         this.express = express();
-        this.router = express.Router();
         this.express.use(bodyParser.urlencoded({ extended: false }));
         this.express.use(xmlParser());
-        this.router.get("/", (req, res) => {
-            const shasum = crypto.createHash("sha1");
-            shasum.update([this.serviceID, req.query.timestamp, req.query.nonce].sort().join(""));
-            const signature = shasum.digest("hex");
-            if (signature !== req.query.signature) {
-                return res.status(500).end();
-            }
-            res.status(200).send(req.query.echostr);
-        });
-        this.router.post("/", (req, res) => {
-            this.emit("message", req.body.xml);
-            res.status(200).end();
-        });
-        this.express.use(this.router);
+        this.express.use(router);
     }
 }
 exports.default = WebHookServer;
