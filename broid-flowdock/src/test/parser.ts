77aaa4c5e8b5d49119f2e4e6ae95343dbbcf1148
Issam Hakimi aka Killix <issam.hakimi@gmail.com>
2017-03-27 10:48:07 -0400

    formatting fix

--- broid-flowdock/src/test/parser.ts
+++ broid-flowdock/src/test/parser.ts
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+
 import ava from 'ava';
 import { Parser } from '../core/Parser';
 
