fdd02d2ebbd13de7001d0ebdddd463a412fc7799
Issam Hakimi aka Killix <issam.hakimi@gmail.com>
2017-03-28 15:26:34 -0400

    utils done

---
+++ broid-utils/lib/Logger.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pino = require("pino");
+class Logger {
+    constructor(name, level) {
+        this.pino = pino({
+            level,
+            name,
+        });
+    }
+    error(...messages) {
+        messages.map((message) => this.pino.error(message));
+    }
+    warning(...messages) {
+        messages.map((message) => this.pino.warn(message));
+    }
+    info(...messages) {
+        messages.map((message) => this.pino.info(message));
+    }
+    debug(...messages) {
+        messages.map((message) => this.pino.debug(message));
+    }
+}
+exports.default = Logger;
--- broid-utils/src/Logger.ts
+++ broid-utils/src/Logger.ts
@@ -1,28 +1,46 @@
+/**
+ * @license
+ * Copyright 2017 Broid.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 import * as pino from 'pino';
 
-export default class Logger {
+export default class Logger { // tslint:disable-line:no-default-export
   private pino: any;
 
-  constructor(name, level) {
+  constructor(name: string, level: string) {
     this.pino = pino({
       level,
       name,
     });
   }
 
-  public error(...messages) {
-    messages.map((message) => this.pino.error(message));
+  public error(...messages: any[]) {
+    messages.map((message: any) => this.pino.error(message));
   }
 
-  public warning(...messages) {
-    messages.map((message) => this.pino.warn(message));
+  public warning(...messages: any[]) {
+    messages.map((message: any) => this.pino.warn(message));
   }
 
-  public info(...messages) {
-    messages.map((message) => this.pino.info(message));
+  public info(...messages: any[]) {
+    messages.map((message: any) => this.pino.info(message));
   }
 
-  public debug(...messages) {
-    messages.map((message) => this.pino.debug(message));
+  public debug(...messages: any[]) {
+    messages.map((message: any) => this.pino.debug(message));
   }
 }
