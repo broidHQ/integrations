9fc40af6da53ceaab683f79d184acf544d1f7b35
tenorok <mail@tenorok.ru>
2017-12-17 20:35:34 +0300

    d.ts for groupme

---
+++ broid-groupme/lib/core/WebHookServer.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="bluebird" />
+import * as Promise from 'bluebird';
+import * as express from 'express';
+import { IAdapterHTTPOptions } from './interfaces';
+export declare class WebHookServer {
+    private express;
+    private logger;
+    private httpClient;
+    private host;
+    private port;
+    constructor(options: IAdapterHTTPOptions, router: express.Router, logLevel?: string);
+    listen(): void;
+    close(): Promise<null>;
+    private setupExpress(router);
+}
---
+++ broid-groupme/lib/core/interfaces.d.ts
@@ -0,0 +1,53 @@
+export interface IAdapterHTTPOptions {
+    host: string;
+    port: number;
+}
+export interface IAdapterOptions {
+    token: string;
+    tokenSecret: string;
+    username: string;
+    http?: IAdapterHTTPOptions;
+    logLevel?: string;
+    serviceID?: string;
+}
+export interface IAttachment {
+    type: string;
+    url?: string;
+    lat?: string;
+    lng?: string;
+    name?: string;
+    token: string;
+    placeholder?: string;
+    charmap?: number[][];
+}
+export interface IMessageParams {
+    text: string;
+    attachments: IAttachment[];
+}
+export interface IMessage extends IMessageParams {
+    id: string;
+    source_guid: string;
+    sender_id: string;
+    sender_type: string;
+    created_at: number;
+    user_id: string;
+    group_id: string;
+    name: string;
+    avatar_url: string;
+    system: boolean;
+    favorited_by: string[];
+    recipient_id?: string;
+}
+export interface IMemberParsed {
+    avatar: string;
+    id: string;
+    username: string;
+}
+export interface IGroupParsed {
+    created_at: number;
+    id: string;
+    name: string;
+    members: IMemberParsed[];
+    updated_at: number;
+    type: string;
+}
