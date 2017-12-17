5387c6f8edd270b17d2e39cecb11f0f9fa8defe9
tenorok <mail@tenorok.ru>
2017-12-17 20:28:53 +0300

    d.ts for telegram

---
+++ broid-telegram/lib/core/interfaces.d.ts
@@ -0,0 +1,11 @@
+export interface IAdapterHTTPOptions {
+    host: string;
+    port: number;
+}
+export interface IAdapterOptions {
+    token: string;
+    webhookURL: string;
+    logLevel?: string;
+    http?: IAdapterHTTPOptions;
+    serviceID?: string;
+}
