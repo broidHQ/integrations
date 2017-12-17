87cf77dd2206c44895fa0958cf64f853fb2be648
tenorok <mail@tenorok.ru>
2017-12-17 20:39:37 +0300

    d.ts for messenger

---
+++ broid-messenger/lib/core/interfaces.d.ts
@@ -0,0 +1,16 @@
+export interface IAdapterHTTPOptions {
+    host: string;
+    port: number;
+}
+export interface IAdapterOptions {
+    token: string;
+    tokenSecret: string;
+    consumerSecret: string;
+    logLevel?: string;
+    http?: IAdapterHTTPOptions;
+    serviceID?: string;
+}
+export interface IWebHookEvent {
+    response: any;
+    request: any;
+}
